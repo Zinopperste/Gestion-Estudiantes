86a1a5d74fcb671e740ec85b7a9734ba07e2e32d
Zinopperste <zinopperste@gmail.com>
2024-10-25 00:54:02 -0300

    Update lista gestion estudiantes
    Agregado el json en el cual se guardan los nombres de los estudiantes y la funcion de guardarlos y cargarlos en script.

--- gestion/gestion_estudiantes.py
+++ gestion/gestion_estudiantes.py
@@ -1,7 +1,20 @@
 """Lista estudiantes"""
-listaEstudiantes = []
+import json
 
-def agregar_estudiante (lista):
+def agregar_estudiante ():
+    # Abrir json y cargar datos de la lista estudiantes
+    try:
+        with open('gestion/gestion_estudiantes.json', 'r', encoding='utf-8') as archivo:
+            listaEstudiantes = json.load(archivo)
+    except (FileNotFoundError, json.JSONDecodeError):
+        listaEstudiantes = []
+
+    # Pedir el nombre del estudiante
     nombre_estudiante = input("Ingrese el nombre del estudiante: ")
-    lista.append(nombre_estudiante)
-    print(f"El estudiante {nombre_estudiante} a sido agregado con exito")
+    listaEstudiantes.append(nombre_estudiante)
+
+    # Guardar la lista actualizada en el archivo
+    with open('gestion/gestion_estudiantes.json', 'w', encoding='utf-8') as archivo:
+        json.dump(listaEstudiantes, archivo, ensure_ascii=False, indent=4)
+
+    print(f"El estudiante {nombre_estudiante} ha sido agregado con exito.")

9e7bcb3fdac2ad2565822e84675793eefdf15493
Zinopperste <zinopperste@gmail.com>
2024-10-25 00:12:58 -0300

    Update gestion_estudiantes.py

--- gestion/gestion_estudiantes.py
+++ gestion/gestion_estudiantes.py
@@ -5,6 +5,3 @@ def agregar_estudiante (lista):
     nombre_estudiante = input("Ingrese el nombre del estudiante: ")
     lista.append(nombre_estudiante)
     print(f"El estudiante {nombre_estudiante} a sido agregado con exito")
-
-agregar_estudiante(listaEstudiantes)
-print(listaEstudiantes)

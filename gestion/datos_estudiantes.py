a50c7b1dc01585e04b6c738b89e4551f5465c3e4
Zinopperste <zinopperste@gmail.com>
2024-10-30 01:02:36 -0300

    Move functions JSON to datos_estudiantes.py

--- gestion/datos_estudiantes.py
+++ gestion/datos_estudiantes.py
@@ -1,1 +1,16 @@
 """Datos Estudiantes"""
+import json
+
+def cargar_datos():
+    try:
+        with open('gestion/gestion.json', 'r', encoding='utf-8') as archivo:
+            campos = json.load(archivo)
+            return [tuple(estudiante) for estudiante in campos.get("estudiantes", [])]
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+    
+def guardar_datos(lista):
+    # Guardar la lista actualizada en el archivo
+    with open('gestion/gestion.json', 'w', encoding='utf-8') as archivo:
+        estudiantes_json = [list(estudiante) for estudiante in lista]
+        json.dump({"estudiantes": estudiantes_json }, archivo, ensure_ascii=False, indent=4)
--- gestion/gestion_estudiantes.py
+++ gestion/gestion_estudiantes.py
@@ -1,21 +1,6 @@
 """Lista estudiantes"""
-import json
 import pandas as pd
-
-def cargar_datos():
-    try:
-        with open('gestion/gestion.json', 'r', encoding='utf-8') as archivo:
-            campos = json.load(archivo)
-            return [tuple(estudiante) for estudiante in campos.get("estudiantes", [])]
-    except (FileNotFoundError, json.JSONDecodeError):
-        return []
-    
-def guardar_datos(lista):
-    # Guardar la lista actualizada en el archivo
-    with open('gestion/gestion.json', 'w', encoding='utf-8') as archivo:
-        estudiantes_json = [list(estudiante) for estudiante in lista]
-        json.dump({"estudiantes": estudiantes_json }, archivo, ensure_ascii=False, indent=4)
-
+from datos_estudiantes import*
 
 def agregar_estudiante(lista):
     # Pedir el nombre del estudiante
